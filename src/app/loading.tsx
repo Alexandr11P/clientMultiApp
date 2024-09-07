9ed44a20fb8af66bd2e67756a4a380fbdff880ea
Alexandr11P <alexlevelup888@gmail.com>
2024-09-07 11:02:54 +0700

    adaptive mobile(chat)

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import Spinner from '@/components/UI/Loaders/Spinner/Spinner'
+import React from 'react'
+
+function Loading() {
+    return (
+        <Spinner />
+    )
+}
+
+export default Loading
